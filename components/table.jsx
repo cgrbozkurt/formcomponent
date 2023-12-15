fb8eb1963dcc850a567d65cfdba923a8e16c2876
cgrbozkurt <cagribozzkurt@gmail.com>
2023-12-16 01:18:51 +0300

    animations

--- components/table.jsx
+++ components/table.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react";
 import { tableHead } from "./thead";
 import { TbArrowsSort } from "react-icons/tb";
 import Tabletd from "./Tabletd";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-import styles from "../styles/animation.module.css";
 import { tableBody } from "./tbody";
 
 const Table = ({ search, setUserForm }) => {
@@ -15,7 +13,6 @@ const Table = ({ search, setUserForm }) => {
   const [displayedTableData, setDisplayedTableData] = useState([]);
 
   const numericColumns = ["price"];
-console.log(tableBody)
 
   useEffect(() => {
     updateDisplayedTableData();
@@ -23,17 +20,17 @@ console.log(tableBody)
 
   const sortTable = (columnName) => {
     const sortedData = [...tableData].sort((a, b) => {
-      const valueA = isNumericColumn(columnName) ? a[columnName] : a[columnName].toLowerCase();
-      const valueB = isNumericColumn(columnName) ? b[columnName] : b[columnName].toLowerCase();
+      const valueA = isNumericColumn(columnName) ? a[columnName] : (a[columnName]?.toLowerCase() || '');
+      const valueB = isNumericColumn(columnName) ? b[columnName] : (b[columnName]?.toLowerCase() || '');
       return valueA > valueB ? -1 : valueA < valueB ? 1 : 0;
     });
     setTableData(sortedData);
   };
-
+  
   const reverseSortTable = (columnName) => {
     const sortedData = [...tableData].sort((a, b) => {
-      const valueA = isNumericColumn(columnName) ? a[columnName] : a[columnName].toLowerCase();
-      const valueB = isNumericColumn(columnName) ? b[columnName] : b[columnName].toLowerCase();
+      const valueA = isNumericColumn(columnName) ? a[columnName] : (a[columnName]?.toLowerCase() || '');
+      const valueB = isNumericColumn(columnName) ? b[columnName] : (b[columnName]?.toLowerCase() || '');
       return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
     });
     setTableData(sortedData);
@@ -42,14 +39,14 @@ console.log(tableBody)
   const handleSort = (columnName) => {
     const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
     setSortOrder(newSortOrder);
-
+  
     if (newSortOrder === "asc") {
       sortTable(columnName);
     } else {
       reverseSortTable(columnName);
     }
   };
-
+  
   const isNumericColumn = (colName) => {
     return numericColumns.includes(colName);
   };
@@ -72,7 +69,6 @@ console.log(tableBody)
     updateDisplayedTableData();
   };
 
-
   const handleNextPage = () => {
     const totalPages = Math.ceil(tableData.length / itemsPerPage);
     if (currentPage < totalPages) {
@@ -94,49 +90,46 @@ console.log(tableBody)
   };
 
   return (
-    <div className="">
-      <button onClick={handleDeleteSelected}>Delete Selected Rows</button>
-
-      <TransitionGroup component={null}>
-        <CSSTransition key={`page-${currentPage}`} timeout={300} classNames={styles}>
-          <table className="w-full">
-            <thead className="bg-gray-100">
-              <tr>
-                <th className="py-4 text-center px-2">
-                  <input type="checkbox" onChange={() => setSelectedRows([])} />
+    <div>
+    <button onClick={handleDeleteSelected}>Delete Selected Rows</button>
+  
+        <table className="w-full transition-all duration-1000">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="py-4 text-center px-2">
+                <input type="checkbox" onChange={() => setSelectedRows([])} />
+              </th>
+              {tableHead.map((thead, index) => (
+                <th className="py-4" key={index}>
+                  <div className="flex items-center gap-5">
+                    {thead} <TbArrowsSort onClick={() => handleSort(thead.toLowerCase())} className="cursor-pointer" />
+                  </div>
                 </th>
-                {tableHead.map((thead, index) => (
-                  <th className="py-4" key={index}>
-                    <div className="flex items-center gap-5">
-                      {thead} <TbArrowsSort onClick={() => handleSort(thead.toLowerCase())} className="cursor-pointer" />
-                    </div>
-                  </th>
-                ))}
-                <th className="py-4 text-start w-[20%]"></th>
-              </tr>
-            </thead>
-            <Tabletd
-              tableData={displayedTableData}
-              search={search}
-              setTableData={setTableData}
-              setUserForm={setUserForm}
-              selectedRows={selectedRows}
-              handleCheckboxChange={handleCheckboxChange}
-            />
-          </table>
-        </CSSTransition>
-      </TransitionGroup>
+              ))}
+              <th className="py-4 text-start w-[20%]"></th>
+            </tr>
+          </thead>
+          <Tabletd
+            tableData={displayedTableData}
+            search={search}
+            setTableData={setTableData}
+            setUserForm={setUserForm}
+            selectedRows={selectedRows}
+            handleCheckboxChange={handleCheckboxChange}
+          />
+        </table>
+      
 
-      <div className="flex justify-between mt-4">
-        <button onClick={handlePrevPage} className="cursor-pointer" disabled={currentPage === 1}>
-          Önceki
-        </button>
-        <span>Sayfa {currentPage}</span>
-        <button onClick={handleNextPage} className="cursor-pointer" disabled={displayedTableData.length < itemsPerPage}>
-          Sonraki
-        </button>
-      </div>
+    <div className="flex justify-between mt-4">
+      <button onClick={handlePrevPage} className="cursor-pointer" disabled={currentPage === 1}>
+        Önceki
+      </button>
+      <span>Sayfa {currentPage}</span>
+      <button onClick={handleNextPage} className="cursor-pointer" disabled={displayedTableData.length < itemsPerPage}>
+        Sonraki
+      </button>
     </div>
+  </div>
   );
 };
 
